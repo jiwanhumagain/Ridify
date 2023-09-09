511a9586fe2a5e6ee6807ee99d9d02164706e8ca
Jiwan Humagain <jeevanhumagain@gmail.com>
2023-09-09 19:07:27 +0545

    Map Added using mapBox

--- ridify/lib/main.dart
+++ ridify/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:ridify/mapbox.dart';
+
 
 void main(){
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Text("Lets Start..........."),
-
+      home:MapBoxWidget(),
     ),
   );
 }
